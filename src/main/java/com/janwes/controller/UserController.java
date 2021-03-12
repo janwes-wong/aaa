540e22dbaf972e8c957502dd0c2901abdb01a9fe
janwes <hzwjanwes@outlook.com>
2021-03-12 16:45:47 +0800

    提交代码至本地仓库

---
+++ src/main/java/com/janwes/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.janwes.controller;
+
+/**
+ * @author Janwes
+ * @version 1.0
+ * @package com.janwes.controller
+ * @date 2021/3/12 16:28
+ * @description
+ */
+public class UserController {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
