aa8ccd0d59bc12eb6e1455db64eb04827910a539
janwes <hzwjanwes@outlook.com>
2021-03-12 17:06:53 +0800

    提交代码至本地仓库 2次修改更新

--- src/main/java/com/janwes/controller/UserController.java
+++ src/main/java/com/janwes/controller/UserController.java
@@ -10,5 +10,6 @@ package com.janwes.controller;
 public class UserController {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("how are you");
     }
 }
