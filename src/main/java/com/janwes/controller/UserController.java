40db430f860c233568240ef4ab8b136810dbd16d
janwes <hzwjanwes@outlook.com>
2021-03-13 08:29:17 +0800

    更新提交

--- src/main/java/com/janwes/controller/UserController.java
+++ src/main/java/com/janwes/controller/UserController.java
@@ -11,5 +11,6 @@ public class UserController {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("how are you");
+        System.out.println("im fine");
     }
 }
